ffb082579881778fd2112945d4f038190d1f502b
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-06-21 12:55:12 -0500

    He creado las páginas correspondientes, he organizado las rutas y añadido el searchbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { AboutComponent } from './components/about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'artist/:id', component: ArtistComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
